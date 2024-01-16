bbbc42d46275deab354b0ee451de5df627396212
Tschakki <tschakki@posteo.de>
2024-01-16 10:42:39 +0100

    Fix deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,13 @@ const config = {
   url: 'https://docs.lisk.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lisk-documentation/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'LiskHQ', // Usually your GitHub org/user name.
   projectName: 'lisk-documentation', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -96,7 +97,7 @@ const config = {
             items: [
               {
                 label: 'Documentation',
-                to: '/docs/intro',
+                to: '/',
               },
             ],
           },
