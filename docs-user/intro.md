0a8ca45320739feadee3b007008584bd2ddd2e8e
Muhammad Talha <13951043+TalhaMaliktz@users.noreply.github.com>
2024-06-13 16:18:26 +0500

    Add btns, style text, images, btns

--- docs-user/intro.md
+++ docs-user/intro.md
@@ -5,9 +5,9 @@ slug: /
 ---
 # Overview
 
-In this section, we will walk you through various ways of interacting with Lisk's products and services.
-
 <span style={{display: "grid", alignItems: "center",  columnGap: '5px', backgroundColor:'#4070f41a', borderRadius:'1em', padding:"5%", marginBottom:'2%'}}>
 <h2>Getting started</h2>
-<img src="/img/bannerwhite.svg" alt="Getting started" title="Getting started" style={{ display: 'block', marginLeft: 'auto'}}/> 
+<img src="/img/bannerwhite.svg" alt="Getting started" title="Getting started" style={{ display: 'block', marginLeft: 'auto', marginRight:'-3em'}}/> 
 </span>
+
+In this section, we will walk you through various ways of interacting with Lisk's products and services.
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -6,28 +6,47 @@ import Link from '@docusaurus/Link';
 const FeatureList = [
   {
     title: 'What is Lisk?',
-    Svg: require('/img/lisk-icon-light-footer.svg').default,
+    Svg: require('/img/lisk-light.svg').default,
+    // Svg: require('/img/lisk-icon-dark-footer.svg').default,
     description: (
       <>
-        <Link to="/docs">Lisk</Link> offers a highly efficient, lightning-fast, and easily scalable Layer 2 (L2) network built on Optimism (OP) and secured by Ethereum.
+        <p><Link to="/docs">Lisk</Link> offers a highly efficient, lightning-fast, and easily scalable Layer 2 (L2) network built on Optimism (OP) and secured by Ethereum.</p>
+        <br></br>
+        <br></br>
+        <Link
+          className="button button--secondary button--lg btn-custom"
+          to="/docs/">
+          EXPLORE
+        </Link>
       </>
     ),
   },
   {
     title: 'Build with Lisk',
     Svg: require('/img/build-dark.svg').default,
+    Svg: require('/img/build-light.svg').default,
     description: (
       <>
-        Lisk offers low-cost transactions, EVM compatibility, and support for all major developer frameworks, making it the top choice for developers. Join Lisk to access essential<Link to="/docs/category/building-on-lisk"> technical materials</Link> for your development journey.
+        <p>Lisk offers low-cost transactions, EVM compatibility, and support for all major developer frameworks, making it the top choice for developers. Join Lisk to access essential<Link to="/docs/category/building-on-lisk"> technical materials</Link> for your development journey.</p>
+        <Link
+          className="button button--secondary button--lg btn-custom"
+          to="/docs/">
+          GET STARTED
+        </Link>
       </>
     ),
   },
   {
     title: 'Ecosystem for the future',
-    Svg: require('/img/lisk-ecosystem-dark.svg').default,
+    Svg: require('/img/lisk-ecosystem-light.svg').default,
     description: (
       <>
         <p>Lisk embraces the superchain framework, offering decentralized <Link to="/user/category/governance">governance</Link> and an interoperable ecosystem. We provide <Link to="https://lisk.com/blog/posts/say-hello-to-the-new-Lisk-L2-grant-program">grant programs</Link> to support the development of products and advocate for the principles of a decentralized world and inclusivity.</p>
+        <Link
+          className="button button--secondary button--lg btn-custom"
+          to="/docs/">
+          LEARN MORE
+        </Link>
       </>
     ),
   },
@@ -37,9 +56,10 @@ function Feature({ Svg, title, description }) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <Svg id='lisk-logo' className={styles.featureSvg} role="img" />
+        {/* <Svg className={styles.featureSvg} role="img" /> */}
       </div>
-      <div className="text--center padding-horiz--md">
+      <div className="text--center text-style-custom padding-horiz--md">
         <Heading as="h3">{title}</Heading>
         <p>{description}</p>
       </div>
