c5e7809a5029e1b39f7676b3f51066f2ed5946c7
Tschakki <tschakki@posteo.de>
2024-12-05 13:31:16 +0100

    Update structure

--- sidebars.js
+++ sidebars.js
@@ -30,7 +30,6 @@ const sidebars = {
         'about-lisk/fees'
       ],
     },
-
     {
       type: 'category',
       label: 'Building on Lisk',
@@ -108,24 +107,7 @@ const sidebars = {
             keywords: ['guides','smart contracts','token development','token standards','erc-20','erc-721','erc-1155'],
           },
         },
-        {
-          type: 'category',
-          label: 'Web3 app development',
-          collapsible: true,
-          collapsed: true,
-          items: [
-            'building-on-lisk/web3-app-development/overview',
-            'building-on-lisk/web3-app-development/build-a-web3-mobile-app',
-            'building-on-lisk/web3-app-development/build-a-web3-pwa',
-          ],
-          link: {
-            type: 'generated-index',
-            title: 'Web3 app development',
-            description: 'Guides explainig how to develop Web3 apps on Lisk.',
-            slug: '/category/building-on-lisk/web3-app-development',
-            keywords: ['guides','apps','dapps','app development','web3 app','web3 apps','pwa','mobile app'],
-          },
-        },
+        'building-on-lisk/web3-app-development',
         {
           type: 'category',
           label: 'Using oracle data',
